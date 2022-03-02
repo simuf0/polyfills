727d6f0b8fc35081489067e55a8e696a1e812548
simUf0 <cabos.simon.dev@gmail.com>
2022-03-02 10:05:22 +0100

    v.1.0.2 : implementing types export

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,9 @@ const sprintf_js_1 = require("sprintf-js");
 Array.prototype.empty = function () {
     return this.length == 0;
 };
-String.whitespace = function (size = 1) {
+String.whitespace = function (length = 1) {
     let whitespace = '';
-    for (let i = 0; i < size; i++)
+    for (let i = 0; i < length; i++)
         whitespace += '\u00A0';
     return whitespace;
 };
@@ -16,4 +16,3 @@ String.prototype.empty = function () {
 String.prototype.sprintf = function (...args) {
     return (0, sprintf_js_1.sprintf)(this.toString(), ...args);
 };
-//# sourceMappingURL=index.js.map
